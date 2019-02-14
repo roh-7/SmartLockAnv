b09133f2d949bebebde320e77ede38a39dc74809
roh-7 <rohit.6233@outlook.com>
2019-02-14 16:04:40 +0530

    shared pref init

--- app/src/main/java/com/example/smartlock/MainActivity.java
+++ app/src/main/java/com/example/smartlock/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.smartlock;
 
 import android.content.Context;
-import android.net.wifi.ScanResult;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity
 {
-	List<ScanResult> ssid;
 	String _ssid;
+
+	private static final String PREF_NAME = "SmartLockAnv";
+	private static final int PRIVATE_MODE = 0;
+
+	SharedPreferences sharedPreferences;
+	SharedPreferences.Editor editor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		sharedPreferences = getApplicationContext().getSharedPreferences(PREF_NAME,PRIVATE_MODE);
+		editor = sharedPreferences.edit();
+
 		WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 		WifiInfo info = wifiManager.getConnectionInfo();
 		if (info != null)
@@ -28,5 +35,19 @@ public class MainActivity extends AppCompatActivity
 		}
 		// _ssid now is printing name of SSID in logs
 		Log.v("SSID: ", "ssid: " + _ssid);
+		set_ssid(_ssid);
+	}
+
+	public void set_ssid(String ssid)
+	{
+		editor.putString("SSID",ssid);
+		editor.apply();
+		editor.commit();
+	}
+
+	public String get_ssid()
+	{
+		// default ssid is set to "ssid"
+		return sharedPreferences.getString("SSID","ssid");
 	}
 }
